29a9fa39de90dfbf98d8726fd0c0afbf1557414d
Iwan van der Kleijn <ivanderk@capgemini.com>
2016-06-08 17:53:45 +0200

    fixed run-time error

--- script/devcon.ps1
+++ script/devcon.ps1
@@ -2,32 +2,7 @@
 # Devcon runner & installer 0.1 - Copyright (c) 2016 Capgemini 
 ##################################################################################
 
-$devconhome = "${env:USERPROFILE}/.devcon"
-$devconfile = "${devconhome}/devcon.jar"
-
-function download() {
-  #$url = "https://github.com/oasp/oasp4j/archive/release/2.0.0.zip"
-  $url = "file:///D:/src/devcon/target/devcon.jar"
-  $output = $devconfile
-  $start_time = Get-Date
-
-  Invoke-WebRequest -Uri $url -OutFile $output
-  Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-}
-
-if (Test-Path $devconfile){
-  java -jar $devconfile @args
-} else {
-  echo "Devcon not present in ${devconhome}. Intenting to download and install it."
-  mkdir $devconhome | Out-Null
-  download
-  global:ADD-PATH $devconhome | Out-Null
-  copy $MyInvocation.MyCommand.Path $devconhome | Out-Null
-  copy "${devconhome}/devcon.ps1" "${devconhome}/devon.ps1" | Out-Null
-  echo "Devcon installed in ${devconhome} and added to the %PATH% environment variable"
-  echo "Start a new console and run 'devon help guide' to see available commands"
-}
-
+# for "main" see down
 
 ##################################################################################
 #  function to add to system PATH 
@@ -155,3 +130,33 @@ Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Co
 Return $NewPath 
 } 
 } 
+
+##############################################################################################################################
+#     Main
+##############################################################################################################################
+
+$devconhome = "${env:USERPROFILE}/.devcon"
+$devconfile = "${devconhome}/devcon.jar"
+
+function download() {
+  #$url = "https://github.com/oasp/oasp4j/archive/release/2.0.0.zip"
+  $url = "file:///D:/src/devcon/target/devcon.jar"
+  $output = $devconfile
+  $start_time = Get-Date
+
+  Invoke-WebRequest -Uri $url -OutFile $output
+  Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+}
+
+if (Test-Path $devconfile){
+  java -jar $devconfile @args
+} else {
+  echo "Devcon not present in ${devconhome}. Intenting to download and install it."
+  mkdir $devconhome | Out-Null
+  download
+  ADD-PATH $devconhome | Out-Null
+  copy $MyInvocation.MyCommand.Path $devconhome | Out-Null
+  copy "${devconhome}/devcon.ps1" "${devconhome}/devon.ps1" | Out-Null
+  echo "Devcon installed in ${devconhome} and added to the %PATH% environment variable"
+  echo "Start a new console and run 'devon help guide' to see available commands"
+}
