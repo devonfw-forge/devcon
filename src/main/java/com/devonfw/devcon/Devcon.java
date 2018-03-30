81bd09b7d860eb5a92e9a0aab80b04a7a24c8fea
CORP\vapadwal <vaibhav.padwal@capgemini.com>
2018-03-30 14:36:06 +0530

    #125: Release version 1.4.2

--- src/main/java/com/devonfw/devcon/Devcon.java
+++ src/main/java/com/devonfw/devcon/Devcon.java
@@ -32,7 +32,7 @@ public class Devcon {
   /**
    * current version of the tool
    */
-  public static final String VERSION = "1.4.1";
+  public static final String VERSION = "1.4.2";
 
   /**
    * url where the versions configuration file is located
