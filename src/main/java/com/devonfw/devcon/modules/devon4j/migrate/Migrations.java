8842aa9fa491976e5a89605aaf2cbae3a37d42af
Rohitkumar <rai.rohitkumar@capgemini.com>
2019-06-26 12:31:14 +0530

    Removed Spring Boot Version

--- src/main/java/com/devonfw/devcon/modules/devon4j/migrate/Migrations.java
+++ src/main/java/com/devonfw/devcon/modules/devon4j/migrate/Migrations.java
@@ -115,7 +115,6 @@ public class Migrations {
         .and() //
         .next().to(VersionIdentifier.ofDevon4j("3.1.0")) //
         .pom().replaceProperty("devon4j.version", "3.1.0") //
-        .replaceProperty("spring.boot.version", "2.1.4.RELEASE") //
         // only for oasp4j legacy project (flyway.version not present in projects created from devon4j)
         .replaceProperty("flyway.version", "5.2.4") //
         .and().java()
